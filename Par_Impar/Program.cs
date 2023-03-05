ae0c4a7cb48166a2273ba9c1bdd7f3152c1043cd
Willian Marioti <willian.marioti@gmail.com>
2023-03-05 19:06:34 -0300

    Add project files.

---
+++ Par_Impar/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x;
+
+        Console.WriteLine("Informe um número: ");
+        x = int.Parse(Console.ReadLine());
+
+        if (x % 2 == 0)
+        {
+            Console.WriteLine("O número é par.");
+        }
+        else
+        {
+            Console.WriteLine("O número é ímpar.");
+        }
+    }
+}
